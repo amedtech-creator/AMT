53c4b167464ce7eff4abe3c8770f057a5b18adcb
amedtech-creator <amedtech@gmail.com>
2026-04-17 00:32:59 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,4 @@
-const CACHE_NAME = 'amt-v3-final';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/orders-admin-amt.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
+const CACHE_NAME = 'amt-v5';
+const ASSETS = ['/', '/index.html', '/orders-admin-amt.html'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
