f2e235b25fa0739874f9f8df5c40b21b98bdb69e
amedtech-creator <amedtech@gmail.com>
2026-04-17 13:13:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE_NAME = 'amt-v5';
-const ASSETS = ['/', '/index.html', '/orders-admin-amt.html'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const V='v10'; self.addEventListener('install', () => self.skipWaiting());
