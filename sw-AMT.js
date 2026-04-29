953d3de2836493847788ac4d683e8f9ff3737f65
amedtech-creator <amedtech@gmail.com>
2026-04-29 15:20:46 +0900

    Add files via upload

--- sw-AMT.js
+++ sw-AMT.js
@@ -1,5 +1,5 @@
-// AMT 알림 Service Worker v20260429-v23
-const CACHE = 'amt-v20260429-v23';
+// AMT 알림 Service Worker v20260429-v24
+const CACHE = 'amt-v20260429-v24';
 const APP_ROOT = '/AMT';
 const ASSETS = [
   APP_ROOT + '/',
